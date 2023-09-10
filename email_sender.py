97c4b28d8830bc0d9a799107b2c741f78038b375
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-10 07:14:23 +0300

    Sending Emails With Python 2

--- email_sender.py
+++ email_sender.py
@@ -1,2 +1,22 @@
 import smtplib
-from email.messages import EmailMessage
+from email.message import EmailMessage
+
+email = EmailMessage()
+
+email['from'] = 'BAAF Company'
+email['to'] = 'abdoulfaridbassirou7898@gmail.com'
+email['subject'] = 'Promotion'
+
+email.set_content('''
+      Dear Founder,
+
+      BAAF Company has achieved a remarkable promotion and has grown significantly!
+
+      kind regards
+	''')
+
+with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
+	smtp.ehlo()
+	smtp.starttls() # tls is an encryption mecanism, and allows us to connect securely to the server.
+	smtp.login('<your email address>', '<your password>')
+	smtp.send_message(email)
